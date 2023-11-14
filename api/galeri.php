b5f21a62f3e77ff030dee807e51d05622580c309
Fajar Ihsana <67202122+Zynfinity@users.noreply.github.com>
2023-11-14 14:27:51 +0700

    Add files via upload

---
+++ api/galeri.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>Linda Nurul Farida-11220123</title>
+</head>
+<body>
+<table border='1' width='100%' height='100' cellspacing='0' cellpadding='0'>
+<tr><td colspan='2' height='150' align='center'><img src='header2.png' width='100%' height='150'</td></tr>
+<tr><td valign='top' bgcolor='#FFF0F5'>
+<table bgcolor='#FAF0E6' border='2' width='100%' cellspacing='0' cellpadding='5'>
+	<tr>
+	<th>Menu</a></th>
+	</tr>
+	<tr>
+	<td><a href='index.php'>Beranda</td>
+	</tr>
+	<tr>
+	<td><a href='CV.php'>CV</a></td>
+	</tr>
+	<td><a href='galeri.php'>Galeri</a></td>
+	</tr>
+	<tr></table>
+	<td width='1700' align='center'bgcolor='#FFF0F5'>
+<img src='yoongi1.jpg'  width='50%' height='50%'>
+<img src='yoon2.jpg'  width='50%' height='50%'>
+<img src='yoon3.jpg'  width='50%' height='50%'>
+<img src='yoon4.jpg'  width='50%' height='50%'>
+</td>
+<tr> <td colspan='2' height='50' align='center'><img src='header2.png' width='100%' height='150'</td></tr>
+
+</table>
+</body>
+</html>
---
+++ api/index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>Linda Nurul Farida-11220123</title>
+</head>
+<body>
+<table border='1' width='100%' height='100' cellspacing='0' cellpadding='0'>
+<tr><td colspan='2' height='150' align='center'><img src='header2.png' width='100%' height='150'</td></tr>
+<tr><td valign='top' bgcolor='#FFF0F5'>
+<table bgcolor='#FAF0E6' border='2' width='100%' cellspacing='0' cellpadding='5'>
+	<tr>
+	<th>Menu</a></th>
+	</tr>
+	<tr>
+	<td><a href='index.php'>Beranda</td>
+	</tr>
+	<tr>
+	<td><a href='CV.php'>CV</a></td>
+	</tr>
+	<td><a href='galeri.php'>Galeri</a></td>
+	</tr>
+	</tr></table>
+	<td width='1700' value='center' align='center'bgcolor='#FFF0F5'>SELAMAT DATANG^^</td>
+		
+	</td>
+<tr><td colspan='2' height='50' align='center'><img src='footer2.png' width='100%' height='150'</td></tr>
+<footer>
+
+
+</table>
+</body>
+</html>
